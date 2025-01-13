b9e04cc9ee1b68ff0dff9fad5c6884defa598792
YuJing949 <Zoesyj1130@outlook.com>
2025-01-13 13:48:32 +0000

    Update drawclock.js

--- drawclock.js
+++ drawclock.js
@@ -1,19 +1,34 @@
+let circles = [];
+
 function setup() {
     createCanvas(600, 600);
     background(0);
-    
+
     let clearButton = createButton('Clear Canvas');
     clearButton.position(10, 10);
     clearButton.mousePressed(() => {
         background(0); // Clear the canvas
+        circles = []; // Clear all stored circles
     });
 }
 
-function draw () {
-    
+function draw() {
+    background(0);
+
+    // Draw all circles currently in the array
     noStroke();
     fill(255);
+    for (let i = circles.length - 1; i >= 0; i--) {
+        let c = circles[i];
+        if (millis() - c.time > 30000) {
+            circles.splice(i, 1); // Remove circle after 30 seconds
+        } else {
+            circle(c.x, c.y, 15);
+        }
+    }
+
+    // Add new circle when mouse is pressed
     if (mouseIsPressed && mouseX < 600 && mouseY < 600 && mouseX > 0 && mouseY > 0) {
-        circle(mouseX, mouseY, 15);
-    };
+        circles.push({ x: mouseX, y: mouseY, time: millis() });
+    }
 }
